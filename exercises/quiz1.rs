9e83436a47c02d5963837f865026407ceaefa7c3
TrULeR <shaivik@antiersolutions.com>
2022-10-10 12:52:14 +0530

    quiz1.rs

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -10,8 +10,6 @@
 // Write a function that calculates the price of an order of apples given
 // the quantity bought. No hints this time!
 
-// I AM NOT DONE
-
 // ez pz
 // Put your function here!
 fn calculate_price_of_apples( num: u32 ) -> u32 {
