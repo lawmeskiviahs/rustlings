36eb73a3361e160ff5a57dc5564e64f49d86c7ab
TrULeR <shaivik@antiersolutions.com>
2022-10-11 14:18:37 +0530

    move_semantics6.rs

--- exercises/move_semantics/move_semantics6.rs
+++ exercises/move_semantics/move_semantics6.rs
@@ -2,21 +2,26 @@
 // Execute `rustlings hint move_semantics6` or use the `hint` watch subcommand for a hint.
 // You can't change anything except adding or removing references.
 
-// I AM NOT DONE
-
 fn main() {
     let mut data = "Rust is great!".to_string();
 
     get_char(&data);
 
     string_uppercase(data);
+
+    // this line will give an error because the ownership of the 'data' variable has been transfered to the
+    // string_uppercase function and was dropped as nothinh was returned and no reference to the variable was used
+    // hence, when the string_uppercase function finishes it's execution, the value of the data variable is dropped
+    // println!("{}", data);
 }
 
+// only takes an immutable reference to the data variable and hence, the value of the variable is not dropped
 // Should not take ownership
 fn get_char(data: &String) -> char {
     data.chars().last().unwrap()
 }
 
+// the ownership of the data variable is tranfered to this function, and hence after the execution of this function the value of the data variable is dropped
 // Should take ownership
 fn string_uppercase(mut data: String) {
     data = data.to_uppercase();
